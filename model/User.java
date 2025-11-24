cde35d48053e7146d4e9791ea322103188c1b6fa
code warrior <someshchaudhary843347@gmail.com>
2025-11-24 17:01:04 +0530

    Create User.java

---
+++ model/User.java
@@ -0,0 +1,24 @@
+package model;
+
+public class User {
+    private String username;
+    private String role; // "admin" or "user"
+
+    public User(String username, String role) {
+        this.username = username;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public String toString() {
+        return username + " (" + role + ")";
+    }
+}
